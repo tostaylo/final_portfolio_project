6c7c25c68c4e702aa97fea2ec7845d512d713450
tostaylo <tostaylo1@gmail.com>
2016-06-08 13:03:38 -0700

    Saving html notes
    working on div’s coming in at the right time

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 $(document).scroll(function () {
     //the current height
     var y = $(this).scrollTop();
-
+//height of top div
+    var headerImage =  document.getElementById('header-image');
+    var headerImageHeight = headerImage.offsetHeight - 400;
+    var headerImageHeight2 = headerImageHeight +1;
     
-    if(y >= 100 && y < 200){
+     
+    
+    if(y >= headerImageHeight && y < 1000){
       
-        $('#side-scroller').show().addClass('come-in');
+        $('#come-in-div-one').show().addClass('come-in');
+        
+        }else if (y >= headerImageHeight2) {
          $('#right-side-scroller').show().addClass('come-in-2');
-    }else if (y >= 300){
+      
+    }else if (y >= headerImageHeight){
         
         $('#bottom-side-scroller').show().addClass('come-in');
          $('#bottom-right-side-scroller').show().addClass('come-in-2');
         
     } else if (y >= 1600) 
-        $('#bottom-scroller').slideUp();
+        console.log('something');
+});
+
+$(window).scroll(function() {
+    if ($(document).height() <= ($(window).height() + $(window).scrollTop())) {
+        //Bottom Reached
+        $('#bottom-scroller').slideDown('slow'); 
+    }
 });
