2d63a8b9ed877c84b74624a703d15073320a94c0
tostaylo <tostaylo1@gmail.com>
2016-06-16 08:17:21 -0700

    Aligned arrows, Made more page
    added media query at 768px for flex box on project info
    added anchors
    added resume pdf

--- js/main.js
+++ js/main.js
@@ -18,25 +18,16 @@ $(document).scroll(function () {
     if(y < headerImageHeight) {
         $('#sticky-header').hide('slow');
         
-        
-          
-        
-        } else if(y >= headerImageHeight && y < headerImageHeight2){
-      
-            $('#come-in-div-one').css('opacity', '1').addClass('come-in');
+         } else if(y >= headerImageHeight && y < headerImageHeight2){
+             $('#come-in-div-one').css('opacity', '1').addClass('come-in');
             $('#sticky-header').show('slow');
-        
         }  else if (y >= headerImageHeight2 && y < headerImageHeight3) {
             $('#come-in-div-two').css('opacity', '1').addClass('come-in-2');
-      
-        } else if (y >= headerImageHeight3 && y < headerImageHeight4) {
-        
-            $('#come-in-div-three').css('opacity', '1').addClass('come-in');
-        
-         } else if (y >= headerImageHeight4){
+      } else if (y >= headerImageHeight3 && y < headerImageHeight4) {
+        $('#come-in-div-three').css('opacity', '1').addClass('come-in');
+        } else if (y >= headerImageHeight4){
              $('#come-in-div-four').css('opacity', '1').addClass('come-in-2');
-        
-    } 
+        } 
 });
 
  /************************SHOW HEADER MENU*************************/
@@ -54,22 +45,19 @@ $('#right-project').stop().fadeIn('slow');
 } , 
                         
                         function() {
-        
-    $(this).animate({width: '50px'}, 500);
+         $(this).animate({width: '50px'}, 500);
     $('#right-project').stop().hide('fast');
-    
-    
-});
+    });
 
 $('#left-arrow').hover(function(){
-    $(this).animate({width: '150px'}, 500)} , 
+    $(this).animate({width: '150px'}, 300);
+      $('#left-project').stop().fadeIn('slow');
+} , 
                         
                         function() {
-        
-    $(this).animate({width: '50px'}, 500);
-    
-    
-});
+        $(this).animate({width: '50px'}, 500);
+     $('#left-project').stop().hide('fast');
+    });
 
 
 
